c024de7c84873c3f3b512ed598da9c90a53fcbbb
Your Name <you@example.com>
2024-09-25 00:35:29 +0530

    Initial commit

---
+++ chatbot.js
@@ -0,0 +1,12 @@
+
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '66f2694b5b495ead28f3dcc6' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production'
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
---
+++ script.js
@@ -0,0 +1,6 @@
+// Example interaction
+document.querySelectorAll('.property button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Property details will be available soon!');
+    });
+});
